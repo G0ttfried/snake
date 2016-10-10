26c15442f4b63034a052188601ca63669e691e63
Gottfried <frdeswaq92@yahoo.com>
2016-10-10 21:25:58 +0300

    create methode to draw a symbol

--- snake/Program.cs
+++ snake/Program.cs
@@ -11,5 +11,11 @@ namespace Snake
         static void Main(string[] args)
         {
         }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
